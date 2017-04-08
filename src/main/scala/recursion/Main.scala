67c0ef7084f54cde5f138bd8f57b41954ad504ce
Lucas Rodrigues <lucas.chapola@gmail.com>
2017-04-08 20:44:33 -0300

    +recursion - sum and sum tail

--- src/main/scala/recursion/Main.scala
+++ src/main/scala/recursion/Main.scala
@@ -41,4 +41,17 @@ object Main {
     balanced(chars, 0)
   }
 
+  def sum(f: Int => Int, a: Int, b: Int): Int =
+    if (a > b) 0
+    else f(a) + sum(f, a + 1, b)
+
+  def sumTail(f: Int => Int, a: Int, b: Int): Int = {
+    def loop(a: Int, acc: Int): Int = {
+      if (a > b) acc
+      else loop(a + 1, acc + f(a))
+    }
+
+    loop(a, 0)
+  }
+
 }
