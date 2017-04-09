ec39523c6c27a35038e6c5bc0359147deb541403
Lucas Rodrigues <lucas.chapola@gmail.com>
2017-04-09 17:54:31 -0300

    +currying

---
+++ src/main/scala/recursion/Currying.scala
@@ -0,0 +1,22 @@
+package recursion
+
+object Currying {
+  def main(args: Array[String]): Unit = {
+    println(sumInts(3, 4))
+    println(sumCubes(3, 4))
+
+  }
+
+  def sumInts = sum(x => x)
+
+  def sumCubes = sum(x => x * x * x)
+
+  def sum(f: Int => Int): (Int, Int) => Int = {
+    def sumf(a: Int, b: Int): Int =
+      if (a > b) 0
+      else f(a) + sumf(a + 1, b)
+
+    sumf
+  }
+
+}
