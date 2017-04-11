22c7c924caada585255025b9c2f061f49df7a081
Lucas Rodrigues <lucas.chapola@gmail.com>
2017-04-10 22:11:16 -0300

    +update - currying

--- src/main/scala/main/Currying.scala
+++ src/main/scala/main/Currying.scala
@@ -3,13 +3,19 @@ package main
 object Currying {
   def main(args: Array[String]): Unit = {
     println(sumInts(3, 4))
+    println(sum(x => x)(3, 4))
+    println(sum2(x => x)(3, 4))
+
     println(sumCubes(3, 4))
+
 
   }
 
   def sumInts = sum(x => x)
 
   def sumCubes = sum(x => x * x * x)
+
+  def sumInts2 = sum2(x => x)
 
   def sum(f: Int => Int): (Int, Int) => Int = {
     def sumf(a: Int, b: Int): Int =
@@ -18,5 +24,9 @@ object Currying {
 
     sumf
   }
+
+  def sum2(f: Int => Int)(a: Int, b: Int): Int =
+    if (a > b) 0
+    else f(a) + sum2(f)(a + 1, b)
 
 }
