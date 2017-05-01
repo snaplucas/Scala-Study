49ce0a80ea5ed80b5d449042da44de9a552a903c
Lucas Rodrigues <lucas.chapola@gmail.com>
2017-04-30 23:45:57 -0300

    +pattern matching - example

--- src/main/scala/main/PatternMatching.scala
+++ src/main/scala/main/PatternMatching.scala
@@ -2,15 +2,18 @@ package main
 
 object PatternMatching {
 
-  trait Expr {
-    def eval(e: Expr): Int = e match {
-      case Number(n) => n
-      case Sum(e1, e2) => eval(e1) + eval(e2)
-    }
-  }
+  trait Expr
 
   case class Number(n: Int) extends Expr
 
   case class Sum(e1: Expr, e2: Expr) extends Expr
-  
+
+  def eval(e: Expr): Int = e match {
+    case Number(n) => n
+    case Sum(e1, e2) => eval(e1) + eval(e2)
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(eval(Sum(Number(1), Number(2))))
+  }
 }
