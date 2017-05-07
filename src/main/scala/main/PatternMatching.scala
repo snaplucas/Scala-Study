b289bf69829db5734f5d4fbfa8c607ffc910c839
Lucas Rodrigues <lucas.chapola@gmail.com>
2017-05-07 20:22:17 -0300

    +pattern matching - isort

--- src/main/scala/main/PatternMatching.scala
+++ src/main/scala/main/PatternMatching.scala
@@ -17,6 +17,11 @@ object PatternMatching {
     println(eval(Sum(Number(1), Number(2))))
   }
 
+  def isort(xs: List[Int]): List[Int] = xs match {
+    case List() => List()
+    case y :: ys => insert(y, isort(ys))
+  }
+
   def insert(x: Int, xs: List[Int]): List[Int] = xs match {
     case List() => List(x)
     case y :: ys => if (x <= y) x :: ys else y :: insert(x, ys)
