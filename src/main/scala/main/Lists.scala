1bb870bc1a4c16372bb075006148b706046ec2ca
Lucas Rodrigues <lucas.chapola@gmail.com>
2017-05-15 21:18:57 -0300

    +Lists - last, init concat and reverse

---
+++ src/main/scala/main/Lists.scala
@@ -0,0 +1,27 @@
+package main
+
+object Lists {
+
+  def last[T](xs: List[T]): T = xs match {
+    case List() => throw new Error("last of empty list")
+    case List(x) => x
+    case _ :: ys => last(ys)
+  }
+
+  def init[T](xs: List[T]): List[T] = xs match {
+    case List() => throw new Error("init of empty list")
+    case List(_) => List()
+    case y :: ys => y :: init(ys)
+  }
+
+  def concat[T](xs: List[T], ys: List[T]): List[T] = xs match {
+    case List() => ys
+    case z :: zs => z :: concat(zs, ys)
+  }
+
+  def reverse[T](xs: List[T]): List[T] = xs match {
+    case List() => xs
+    case y :: ys => reverse(ys) ::: List(y)
+  }
+
+}
